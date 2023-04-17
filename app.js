77554061c344a7d946ede6d79e766f65d93374ed
kcblack22@gmail.com <kcblack22@gmail.com>
2023-04-17 14:59:46 +0100

    The cors issues resolved

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors');
 require('dotenv').config();
 
 
@@ -32,10 +33,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/waiting', waitingRouter);
 app.use('/suggest', suggerstRouter);
+app.use(cors())
 
 
 // catch 404 and forward to error handler
